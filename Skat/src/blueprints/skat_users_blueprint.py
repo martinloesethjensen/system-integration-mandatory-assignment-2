6ba9279b077b2260d0dfb20385fab65ef0d81bc7
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-01-04 23:43:27 +0100

    Made blueprints and fixed UNIQUE constraint with rollback on skat_user creating and update on user_id property

---
+++ Skat/src/blueprints/skat_users_blueprint.py
@@ -0,0 +1,94 @@
+import requests 
+import json
+from flask import request, jsonify, Response, Blueprint
+from datetime import datetime
+
+skat_users_blueprint = Blueprint('skat_users_blueprint', __name__)
+
+
+import src.models
+from app import db
+
+# Get all skat users
+@skat_users_blueprint.route('/api/skat/skat-users', methods=['GET'])
+def get_all_skat_users():
+    all_skat_users = src.models.SkatUsers.query.all()
+    result = src.models.skat_users_schema.dump(all_skat_users)
+    return jsonify(result)
+
+
+# Get single skat user
+@skat_users_blueprint.route('/api/skat/skat-users/<id>', methods=['GET'])
+def get_single_skat_user(id):
+    skat_user = src.models.SkatUsers.query.get(id)
+    if skat_user is None:
+        return Response(response=json.dumps(
+            {"message": "Skat User with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+    return src.models.skat_user_schema.jsonify(skat_user), 200
+
+
+# Delete a skat user
+@skat_users_blueprint.route('/api/skat/skat-users/<id>', methods=['DELETE'])
+def delete_skat_user(id):
+    skat_user = src.models.SkatUsers.query.get(id)
+    if skat_user is None:
+        return Response(response=json.dumps(
+            {"message": "Skat User with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+
+    # Delete user from skat user year table
+    src.models.SkatUsersYears.query.filter_by(skat_user_id=skat_user.id).delete()
+
+    db.session.delete(skat_user)
+    db.session.commit()
+    return src.models.skat_user_schema.jsonify(skat_user), 200
+
+
+# Create skat user
+@skat_users_blueprint.route('/api/skat/skat-users', methods=['POST'])
+def create_skat_user():
+    user_id = request.json['userId']
+    created_at = request.json['createdAt']
+    is_active = request.json['isActive']
+    new_skat_user = src.models.SkatUsers(user_id, created_at, is_active)
+    
+    try:
+        db.session.add(new_skat_user)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return "user_id: {} already exists".format(user_id), 409
+    
+    return src.models.skat_user_schema.jsonify(new_skat_user), 201
+
+
+# Update skat user
+@skat_users_blueprint.route('/api/skat/skat-users/<id>', methods=['PUT'])
+def update_skat_user(id):
+    user_id = request.json['userId']
+    created_at = request.json['createdAt']
+    is_active = request.json['isActive']
+
+    skat_user = src.models.SkatUsers.query.get(id)
+
+    if skat_user is None:
+        return Response(response=json.dumps(
+            {"message": "Skat user with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+
+    if user_id:
+        skat_user.user_id = user_id
+    if created_at:
+        skat_user.created_at = datetime.strptime(
+            created_at, "%Y-%m-%dT%H:%M:%S")
+    if is_active is not None:
+        skat_user.is_active = is_active
+
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return "user_id: {} already exists".format(user_id), 409
+        
+    return src.models.skat_user_schema.jsonify(skat_user), 200
---
+++ Skat/src/blueprints/skat_years_blueprint.py
@@ -0,0 +1,103 @@
+import requests 
+import json
+from flask import request, jsonify, Response, Blueprint
+from datetime import datetime
+
+skat_years_blueprint = Blueprint('skat_years_blueprint', __name__)
+
+import src.models
+from app import db
+
+@skat_years_blueprint.route('/api/skat/skat-years', methods=['GET'])
+def get_all_skat_years():
+    all_skat_years = src.models.SkatYears.query.all()
+    result = src.models.skat_years_schema.dump(all_skat_years)
+    return jsonify(result), 200
+
+
+# Get single skat year
+@skat_years_blueprint.route('/api/skat/skat-years/<id>', methods=['GET'])
+def get_single_skat_year(id):
+    skat_year = src.models.SkatYears.query.get(id)
+    if skat_year is None:
+        return Response(response=json.dumps(
+            {"message": "Skat Year with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+    return src.models.skat_year_schema.jsonify(skat_year), 200
+
+
+# Create skat year
+@skat_years_blueprint.route('/api/skat/skat-years', methods=['POST'])
+def create_skat_year():
+    label = request.json['label']
+    created_at = request.json['createdAt']
+    modified_at = request.json['modifiedAt']
+    start_date = request.json['startDate']
+    end_date = request.json['endDate']
+    is_active = request.json['isActive']
+
+    new_skat_year = src.models.SkatYears(
+        label, created_at, modified_at, start_date, end_date, is_active)
+
+    # Get all skat users and insert into skat users years
+    all_skat_users = src.models.SkatUsers.query.all()
+    for skat_user in all_skat_users:
+        new_skat_user_year = src.models.SkatUsersYears(
+            skat_user.id, new_skat_year.id, skat_user.user_id, False, 0.0)
+        db.session.add(new_skat_user_year)
+
+    db.session.add(new_skat_year)
+    db.session.commit()
+    return src.models.skat_year_schema.jsonify(new_skat_year), 201
+
+
+# Update skat year
+@skat_years_blueprint.route('/api/skat/skat-years/<id>', methods=['PUT'])
+def update_skat_year(id):
+    label = request.json['label']
+    created_at = request.json['createdAt']
+    modified_at = request.json['modifiedAt']
+    start_date = request.json['startDate']
+    end_date = request.json['endDate']
+    is_active = request.json['isActive']
+
+    skat_year = src.models.SkatYears.query.get(id)
+
+    if skat_year is None:
+        return Response(response=json.dumps(
+            {"message": "Skat Year with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+
+    if label:
+        skat_year.label = label
+    if created_at:
+        skat_year.created_at = datetime.strptime(
+            created_at, "%Y-%m-%dT%H:%M:%S")
+    if modified_at:
+        skat_year.modified_at = datetime.strptime(
+            modified_at, "%Y-%m-%dT%H:%M:%S")
+    if start_date:
+        skat_year.start_date = datetime.strptime(
+            start_date, "%Y-%m-%dT%H:%M:%S")
+    if end_date:
+        skat_year.end_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S")
+    if is_active is not None:
+        skat_year.is_active = is_active
+
+    db.session.commit()
+    return src.models.skat_year_schema.jsonify(skat_year), 200
+
+
+# Delete a skat year
+@skat_years_blueprint.route('/api/skat/skat-years/<id>', methods=['DELETE'])
+def delete_skat_year(id):
+    skat_year = src.models.SkatYears.query.get(id)
+    if skat_year is None:
+        return Response(response=json.dumps(
+            {"message": "Skat Year with id {} does not exist!".format(id)}),
+            status=404, mimetype="application/json")
+
+    db.session.delete(skat_year)
+    db.session.commit()
+    return src.models.skat_year_schema.jsonify(skat_year), 200
+
