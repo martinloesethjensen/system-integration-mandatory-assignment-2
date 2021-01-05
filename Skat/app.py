a67e4afd76287b4bc10c72088632498438dee174
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-01-05 20:07:02 +0100

    Use tax blueprint

--- Skat/app.py
+++ Skat/app.py
@@ -17,8 +17,10 @@ db = SQLAlchemy(app)
 from src.models import *
 
 from src.blueprints.skat_users_blueprint import skat_users_blueprint
+from src.blueprints.tax_blueprint import tax_blueprint
 
 app.register_blueprint(skat_users_blueprint)
+app.register_blueprint(tax_blueprint)
 
 
 
@@ -133,73 +135,6 @@ def get_all_skat_users_years():
     all_skat_users_years = SkatUsersYears.query.all()
     result = skat_users_years_schema.dump(all_skat_users_years)
     return jsonify(result), 200
-
-
-# ----------------
-# Pay Taxes
-# ----------------
-
-@app.route('/api/skat/pay-taxes', methods=['POST'])
-def pay_taxes():
-    user_id = request.json['UserId']
-    user_amount = request.json['Amount']
-
-    skat_user_year = SkatUsersYears.query.filter_by(user_id=user_id).first()
-
-    if not skat_user_year:
-        return Response(response=json.dumps(
-            {"message": "Skat User Year with user id {} does not exist!".format(user_id)}),
-            status=404, mimetype="application/json")
-
-    if skat_user_year.is_paid == 1:
-        return Response(response=json.dumps(
-            {"message": "Skat user has paid taxes"}),
-            status=200, mimetype="application/json")
-    else:
-        tax_calc_response = requests.post('http://skat_tax_calculator/api/Skat_Tax_Calculator',
-                                          data=json.dumps(
-                                              {'amount': user_amount}),
-                                          headers={'Content-Type': 'application/json'})
-
-        # Status code is 200
-        if tax_calc_response:
-            json_response = tax_calc_response.json()
-            tax_money = json_response['tax_money']
-
-            # tax_money can't be negative
-            # Return 400 - bad request 'cause of negative number value
-            if tax_money < 0:
-                return Response(response=json.dumps(
-                    {"message": "Negative Value"}),
-                    status=400, mimetype="application/json")
-
-            skat_user_year.amount = tax_money
-            skat_user_year.is_paid = True
-
-            bank_response = requests.post('http://bank_system/api/Money/withdrawl',
-                                          data=json.dumps(
-                                              {'userId': user_id, 'amount': user_amount}),
-                                          headers={'Content-Type': 'application/json'})
-
-            # Status code is 200
-            if bank_response:
-                # Only commit db changes if bank_response is ok
-                db.session.commit()
-                return Response(response=json.dumps(
-                    {"message": "Succeeded in sending request to the bank api"}),
-                    status=200, mimetype="application/json")
-            else:
-
-                return Response(response=json.dumps(
-                    {"message": "Something went wrong with the request for withdrawing money."}),
-                    status=500, mimetype="application/json")
-
-            return skat_user_year_schema.jsonify(skat_user_year)
-        else:
-            return Response(response=json.dumps(
-                {"message": "Something went wrong with the request to Skat Tax Calculator."}),
-                status=500, mimetype="application/json")
-
 
 if __name__ == '__main__':
     app.run(port=5006, host='0.0.0.0', debug=True)
